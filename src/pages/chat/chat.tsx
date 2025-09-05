ba9770e098a385c58ee3348e61fd786e6cdeca51
jhj1819 <72265381+jhj1819@users.noreply.github.com>
2025-09-05 17:03:11 +0900

    fix: "받는 응답 수정정"

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -42,7 +42,7 @@ export function Chat() {
           
           setMessages(prev => [
             ...prev,
-            { content: resultMessage.analysisInfo.intentName, role: "assistant", id: sessionId }
+            { content: resultMessage.fulfillmentText, role: "assistant", id: sessionId }
           ]);
           setIsLoading(false); // 로딩 종료
         }
